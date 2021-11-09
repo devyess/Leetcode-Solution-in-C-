0fbb45820556f39fa21697f84fad7c9bdfd78989
devyess <devyessbuchha@gmail.com>
2021-11-09 23:00:02 +0530

    1672 solution added

---
+++ 1672.richest_customer_wealth.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxi = INT_MIN;
+        for(int i=0;i<accounts.size();i++){
+            int sum=0;
+            for(int j=0;j<accounts[i].size();j++)
+            {
+                sum=sum + accounts[i][j];
+            }
+            if(maxi<sum)
+            {
+                maxi=sum;
+            }
+        }
+        return maxi;
+        
+    }
+};
